0ca3488fe46022005a49b719626426a055705cd0
lat453 <151341442+lat453@users.noreply.github.com>
2024-12-22 20:26:52 +0530

    Create Game Leap Challenge.java

---
+++ 22.12.2024/Hackerrank/Game Leap Challenge.java
@@ -0,0 +1,46 @@
+program:Game Leap Challenge
+import java.util.*;
+
+public class Solution {
+
+    public static boolean canWin(int leap, int[] game) {
+        return isWinnable(leap, game, 0);
+    }
+
+    private static boolean isWinnable(int leap, int[] game, int position) {
+        if (position >= game.length) {
+            return true;
+        }
+        if (position < 0 || game[position] == 1) {
+            return false;
+        }
+        game[position] = 1;
+        return isWinnable(leap, game, position + leap) ||
+               isWinnable(leap, game, position + 1) ||
+               isWinnable(leap, game, position - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+            System.out.println(canWin(leap, game) ? "YES" : "NO");
+        }
+        scan.close();
+    }
+} 
+Input:
+2
+5 3
+0 0 0 0 0
+6 5
+0 0 0 1 1 1
+output:
+YES
+NO
