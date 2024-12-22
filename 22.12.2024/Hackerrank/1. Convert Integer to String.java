847acc190a6cd84fcdfef8f8dd792f83a4960049
lat453 <151341442+lat453@users.noreply.github.com>
2024-12-22 19:14:34 +0530

    Create  Convert Integer to String.java

---
+++ 22.12.2024/Hackerrank/1. Convert Integer to String.java
@@ -0,0 +1,27 @@
+1. Convert Integer to String
+
+Program:
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.close();
+        String s = Integer.toString(n);
+        if (n == Integer.parseInt(s)) {
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer.");
+        }
+    }
+}
+
+Input:
+
+100
+
+Output:
+
+Good job
