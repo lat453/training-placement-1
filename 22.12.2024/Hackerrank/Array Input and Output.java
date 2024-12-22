8a014d189e253b8743b1c9385fa5502bf1acf6ee
lat453 <151341442+lat453@users.noreply.github.com>
2024-12-22 19:30:50 +0530

    Create Array Input and Output.java

---
+++ 22.12.2024/Hackerrank/Array Input and Output.java
@@ -0,0 +1,34 @@
+Program:Array Input and Output
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
+        }
+        scan.close();
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
+Sample Input:
+
+5  
+10  
+20  
+30  
+40  
+50
+
+Sample Output:
+
+10  
+20  
+30  
+40  
+50
+
