093d0ef0a329b05b36b0ad027f2fcaa288893cb2
lat453 <151341442+lat453@users.noreply.github.com>
2024-12-22 13:03:39 +0530

    Create Printing the First N Multiples.java

---
+++ 21.12.2024/Printing the First N Multiples.java
@@ -0,0 +1,33 @@
+Program 8: Printing the First N Multiples
+
+Code (Without Comments):
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+        scanner.close();
+    }
+}
+
+Input:
+
+3
+
+Output:
+
+3 x 1 = 3
+3 x 2 = 6
+3 x 3 = 9
+3 x 4 = 12
+3 x 5 = 15
+3 x 6 = 18
+3 x 7 = 21
+3 x 8 = 24
+3 x 9 = 27
+3 x 10 = 30
