e58f9a403165db7ec24ce8e78b581c3177773a76
lat453 <151341442+lat453@users.noreply.github.com>
2024-12-22 13:19:17 +0530

    Create Check if a Number is "Weird" or "Not Weird" Based on Conditions.java

---
+++ 21.12.2024/Check if a Number is "Weird" or "Not Weird" Based on Conditions.java
@@ -0,0 +1,62 @@
+program :5 Check if a Number is "Weird" or "Not Weird" Based on Conditions
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if (n % 2 == 1 || (n >= 6 && n <= 20)) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
+        scanner.close();
+    }
+}
+
+
+---
+
+Inputs and Outputs:
+
+Input 1:
+
+3
+
+Output 1:
+
+Weird
+
+
+---
+
+Input 2:
+
+24
+
+Output 2:
+
+Not Weird
+
+
+---
+
+Input 3:
+
+8
+
+Output 3:
+
+Not Weird
+
+
+---
+
+Input 4:
+
+15
+
+Output 4:
+
+Weird 
