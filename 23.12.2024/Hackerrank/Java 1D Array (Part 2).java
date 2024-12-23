0b9df8f1e4bc5b6530c2c8ad0d0b04c65378405b
lat453 <151341442+lat453@users.noreply.github.com>
2024-12-23 19:22:28 +0530

    Create Java 1D Array (Part 2).java

---
+++ 23.12.2024/Hackerrank/Java 1D Array (Part 2).java
@@ -0,0 +1,56 @@
+Program: Java 1D Array (Part 2)
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static boolean canWin(int leap, int[] game) {
+        return isSolvable(leap, game, 0);
+    }
+
+    private static boolean isSolvable(int leap, int[] game, int i) {
+        if (i >= game.length) return true;
+        if (i < 0 || game[i] == 1) return false;
+
+        game[i] = 1; // Mark as visited
+
+        return isSolvable(leap, game, i + leap) || 
+               isSolvable(leap, game, i + 1) || 
+               isSolvable(leap, game, i - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+
+            System.out.println(canWin(leap, game) ? "YES" : "NO");
+        }
+        scan.close();
+    }
+}
+Input:
+4
+5 3
+0 0 0 0 0
+6 5
+0 0 0 1 1 1
+6 3
+0 0 1 1 1 0
+3 1
+0 1 0
+
+
+
+output:
+YES
+YES
+NO
+NO
