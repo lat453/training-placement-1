0c762b76162426c2390d97657bbee5010e92a380
lat453 <151341442+lat453@users.noreply.github.com>
2024-12-23 21:22:20 +0530

    Create Calculate the Area of a Parallelogram Using Static Initialization Block.java

---
+++ 22.12.2024/Calculate the Area of a Parallelogram Using Static Initialization Block.java
@@ -0,0 +1,52 @@
+Calculate the Area of a Parallelogram Using Static Initialization Block"
+
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    static int B;
+    static int H;
+    static boolean flag = true;
+
+    static {
+        Scanner scanner = new Scanner(System.in);
+        B = scanner.nextInt();
+        H = scanner.nextInt();
+        if (B <= 0 || H <= 0) {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
+    public static void main(String[] args) {
+        if (flag) {
+            int area = B * H;
+            System.out.print(area);
+        }
+    }
+}
+
+Input/Output:
+
+Input 1:
+
+1
+3
+
+Output 1:
+
+3
+
+Input 2:
+
+-1
+2
+
+Output 2:
+
+java.lang.Exception: Breadth and height must be positive
