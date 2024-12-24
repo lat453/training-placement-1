070c69efa179c423fc85334c9e5fa3b3ba79031a
lat453 <151341442+lat453@users.noreply.github.com>
2024-12-24 19:34:44 +0530

    Create Bufferreader.java

---
+++ 24.12.2024/Bufferreader.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        bufferedReader.close();
+
+        
+        BigInteger number = new BigInteger(n);
+
+        
+        if (number.isProbablePrime(10)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+    }
+}
